7c513849ae5babe7e8b7c891e51048ab0bf49dd4
David <dcsuka@uci.edu>
2022-09-23 20:22:25 -0700

    Some speed improvements but less concise

--- substring-with-concatenation-of-all-words/solution.py
+++ substring-with-concatenation-of-all-words/solution.py
@@ -9,12 +9,12 @@ class Solution:
         matches = [(mat.group(1), mat.start()) for mat in re.finditer(f"(?<!{regex})(?=(({regex})+))", s)]
         for x, start in matches:
             trial = [x[i:i+word_length] for i in range(0, len(x), word_length)]
+            if len(trial) == len(words) and Counter(trial) == Counter(words):
+                ret.append(start)
+                continue
             end = num_words
             while end <= len(trial):
-                test = trial[end-num_words:end]
-                if Counter(test) == Counter(words):
+                if Counter(trial[end-num_words:end]) == Counter(words):
                     ret.append(start+(end-num_words)*word_length)
                 end += 1
-        if len(set(s)) == 1 and ret:
-            ret = list(range(0, max(ret)+1))
         return ret
