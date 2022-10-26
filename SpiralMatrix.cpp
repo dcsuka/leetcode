5f2e63392776036c25d704c57ba2df3abf783d64
David <dcsuka@uci.edu>
2022-10-26 12:51:52 -0700

    Spiral Matrix Changes

--- SpiralMatrix.cpp
+++ SpiralMatrix.cpp
@@ -1,64 +1,52 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
+template <typename T>
+void print(const T &myT)
+{
+    for (auto x : myT)
+        std::cout << x << " ";
+    std::cout << std::endl;
+}
+
 class Solution
 {
 public:
-    std::vector<int> getSpiral(std::vector<std::vector<int>> &matrix)
+    vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
     {
-        // right, down, left, up
-        int rows = matrix.size(), cols = matrix[0].size();
-        int down = 0, right = 0, mode = 0;
-        bool justswitched = false;
-        std::vector<int> ret(rows * cols, 0);
-        std::vector<std::pair<int, int>> increments{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        std::vector<int> limits{cols - 1, rows - 1, 0, 0};
-
-        // finishing right decrements up, finishing down decrements right, finishing left decrements down, finishing up decrements left.
-
-        std::vector<int *> vars{&right, &down, &right, &down};
-        std::cout << "test1" << std::endl;
-        int count = 0;
-        while (true)
+        int m = matrix.size() - 1, n = matrix[0].size(), i = 0, j = -1, mode = 0;
+        int *curDist = &n;
+        vector<int> ret;
+        ret.reserve(m * n);
+        while (*curDist > 0)
         {
-            if (count > 100)
-                break;
-            std::cout << *vars[mode] << " " << limits[mode] << " " << down << " " << right << " " << std::endl;
-            if (*vars[mode] == limits[mode])
+            cout << "m: " << m << ", n: " << n << endl;
+            pair<int, int> directions = dir[mode];
+            cout << "directions: " << directions.first << directions.second << endl;
+            for (int z = 0; z < *curDist; ++z)
             {
-                std::cout << "met" << std::endl;
-                if (justswitched)
-                    break;
-                int targetlimit = (--mode < 0) ? 3 : mode;
-                if (targetlimit < 2)
-                    --limits[targetlimit];
-                else
-                    ++limits[targetlimit];
-                mode = (mode == 3) ? 0 : mode + 1;
-                justswitched = true;
-                continue;
+                i += directions.first;
+                j += directions.second;
+                std::cout << i << j << endl;
+                ret.emplace_back(matrix[i][j]);
             }
-            // std::cout << "test2" << std::endl;
-            justswitched = false;
-            // std::cout << x << " " << y << std::endl;
-            ret[count] = matrix[down][right];
-            down += increments[mode].first;
-            right += increments[mode].second;
-            count++;
+            mode = (mode + 1) % 4;
+            *curDist = *curDist - 1;
+            curDist = (curDist == &n) ? &m : &n;
+            print(ret);
         }
-        std::cout << std::endl;
-        for (auto x : ret) std::cout << x;
-        std::cout << std::endl;
         return ret;
     }
 };
 
 int main()
 {
-    std::vector<std::vector<int>> mat1{
-        std::vector<int>{1, 2, 3, 4}, std::vector<int>{5, 6, 7, 8}, std::vector<int>{9, 10, 11, 12}, std::vector<int>{13, 14, 15, 16}};
+    vector<vector<int>> mat1{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
     Solution s;
-    std::cout << "test" << std::endl;
-    auto x = s.getSpiral(mat1);
+    auto x = s.spiralOrder(mat1);
+    print(x);
     return 0;
 }
