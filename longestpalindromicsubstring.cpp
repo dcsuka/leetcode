2ad9a539be68bf84028022ad61ed40efebef6708
David <dcsuka@uci.edu>
2022-11-07 11:17:14 -0800

    LongestPalindromicSubstring

--- longestpalindromicsubstring.cpp
+++ longestpalindromicsubstring.cpp
@@ -50,7 +50,35 @@ namespace BruteForce
     }
 }
 
+using sst = string::size_type;
+
+namespace TwoPointer
+{
+    class Solution
+    {
+    public:
+        string longestPalindrome(string s);
+    };
+
+    string Solution::longestPalindrome(string s) {
+        sst n = s.size();
+        if (n == 0) return "";
+        string ret(1, s.at(0));
+        for (sst i = 0; i < n; ++i) {
+            sst left = i, right = i;
+            while (left > 0 && s.at(left - 1) == s.at(left)) --left;
+            while (right < n - 1 && s.at(right + 1) == s.at(right)) ++right;
+            while (left > 0 && right < n - 1 && s.at(left - 1) == s.at(right + 1)) --left, ++right;
+            if (right - left + 1 > ret.size()) ret = s.substr(left, right - left + 1);
+        }
+        return ret;
+    }
+}
 int main()
 {
+    BruteForce::Solution bf;
+    cout << bf.longestPalindrome("sdfddbababsdf") << endl;
+    TwoPointer::Solution tp;
+    cout << tp.longestPalindrome("sdfddbababsdf") << endl;
     return 0;
 }
